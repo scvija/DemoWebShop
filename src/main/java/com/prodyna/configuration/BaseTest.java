1472d48f4afff45aaad388efd878d06beb860b0d
scvijanovic <stefan.cvijanovic@prodyna.com>
2020-10-23 14:25:00 +0200

    Added Resources and page object packages, added data.properties file

--- src/main/java/com/prodyna/configuration/BaseTest.java
+++ src/main/java/com/prodyna/configuration/BaseTest.java
@@ -1,4 +1,43 @@
 package com.prodyna.configuration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.asserts.SoftAssert;
+
 public class BaseTest {
+    public WebDriver driver;
+    public SoftAssert softAssert = new SoftAssert();
+
+    public WebDriver initializeDriver() throws IOException {
+
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("src/main/java/resources/data.properties");
+
+        prop.load(fis);
+        String browserName = prop.getProperty("browser");
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        }
+
+        if (browserName.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        return driver;
+    }
 }
