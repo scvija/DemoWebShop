10c2ee599e9c75fd00cd3c37831303609eff9550
scvijanovic <stefan.cvijanovic@prodyna.com>
2020-10-24 21:12:31 +0200

    Added navigateToPAge method to the beforeTest Added negative tests for email and password fields Changed the happy path test

--- src/test/java/com/prodyna/test/RegistrationTest.java
+++ src/test/java/com/prodyna/test/RegistrationTest.java
@@ -1,6 +1,5 @@
 package com.prodyna.test;
 
-import com.prodyna.configuration.BasePage;
 import com.prodyna.configuration.BaseTest;
 import com.prodyna.pageObjects.Header;
 import com.prodyna.pageObjects.RegisterPage;
@@ -9,17 +8,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
-import static com.prodyna.pageObjects.utility.Constants.*;
+import static com.prodyna.utility.Constants.*;
 
 public class RegistrationTest extends BaseTest {
     @BeforeMethod
     public void initialize() throws IOException {
         driver = initializeDriver();
-        driver.get(homepageUrl());
+        navigateToPage(registerPageUrl);
     }
 
     @AfterMethod
@@ -32,11 +29,6 @@ public class RegistrationTest extends BaseTest {
 
         Header header = new Header(driver);
         RegisterPage registerPage = new RegisterPage(driver);
-        SoftAssert softAssert = new SoftAssert();
-
-        clickElement(header.register);
-
-
 
         // trouble with the loop inside the method
         registerPage.verifyRegistrationFormFieldsAreDisplayed();
@@ -47,10 +39,8 @@ public class RegistrationTest extends BaseTest {
 
     @Test
     public void registerMandatoryText() {
-        Header header = new Header(driver);
         RegisterPage registerPage = new RegisterPage(driver);
 
-        clickElement(header.register);
         clickElement(registerPage.registerButton);
         registerPage.verifyRegistrationFormMandatoryText();
 
@@ -59,33 +49,57 @@ public class RegistrationTest extends BaseTest {
     @Test
     public void registerFunctionalityHappyPathTest() {
         Header header = new Header(driver);
-        RegisterPage registerPage = new RegisterPage(driver);
-        SoftAssert softAssert = new SoftAssert();
-        // Could possibly be replaced with driver.get / navigate to url, to make the tests more atomic
-        clickElement(header.register);
-        clickElement(registerPage.registerButton);
+        RegisterPage register = new RegisterPage(driver);
 
-        //Login with a set of data??
-
-        enterText(registerPage.firstNameInput, firstName);
-        enterText(registerPage.lastnameInput, lastName);
+        clickElement(register.registerButton);
 
         long timeStamp = System.currentTimeMillis();
-        String mailAddress = timeStamp + emailLogin;
-        enterText(registerPage.emailInput, mailAddress);
+        String mailAddressAndTime = timeStamp + emailLogin;
 
-        enterText(registerPage.passwordInput, password);
-        enterText(registerPage.confirmedPasswordInput, password);
+        register.fillRegisterForm(firstName,lastName,mailAddressAndTime, password);
+        clickElement(register.registerButton);
 
-//      takes a long time to list through the elements
-        registerPage.mandatoryTextNotDisplayed();
-
-        clickElement(registerPage.registerButton);
-        softAssert.assertEquals(registerPage.resultMessage.getText(), registrationComplete);
-        clickElement(registerPage.continueButton);
+        softAssert.assertEquals(register.resultMessage.getText(), registrationComplete);
+        clickElement(register.continueButton);
 
         String myAccountText = header.myAccount.getText();
-        softAssert.assertEquals(myAccountText, mailAddress);
+        softAssert.assertEquals(myAccountText, mailAddressAndTime);
+
+        softAssert.assertAll();
     }
+
+    @Test
+    public void emailInputNegativeTest(){
+        RegisterPage register = new RegisterPage(driver);
+
+        enterText(register.emailInput, emailInvalid);
+        clickElement(register.registerButton);
+        softAssert.assertEquals(register.emailMandatoryText.getText(),emailWrongForm);
+
+        register.fillRegisterForm(firstName,lastName,emailLogin,password);
+        clickElement(register.registerButton);
+
+        softAssert.assertEquals(register.summaryErrorMessage.getText(),emailExists);
+
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void passwordInputNegativeTest(){
+        RegisterPage register = new RegisterPage(driver);
+
+        enterText(register.passwordInput, passwordShort);
+        clickElement(register.registerButton);
+        softAssert.assertEquals(register.passwordMandatoryText.getText(),passwordTooShort);
+
+        enterText(register.passwordInput,password);
+        enterText(register.confirmedPasswordInput, passwordDifferent);
+        clickElement(register.registerButton);
+        softAssert.assertEquals(register.confirmPasswordMandatoryText.getText(),passwordMissmatch);
+
+        softAssert.assertAll();
+
+    }
+
 
 }
