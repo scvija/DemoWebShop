5cac0ec4cbd4b2bea16fc824d02442bf0715a2f7
scvijanovic <stefan.cvijanovic@prodyna.com>
2020-10-25 21:28:55 +0100

    Added Category navigation test

--- src/test/java/com/prodyna/test/CategoriesTest.java
+++ src/test/java/com/prodyna/test/CategoriesTest.java
@@ -2,14 +2,13 @@ package com.prodyna.test;
 
 import com.prodyna.configuration.BaseTest;
 import com.prodyna.pageObjects.CategoriesMenu;
+import com.prodyna.pageObjects.ProductPage;
 import org.junit.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-
-import static com.prodyna.utility.Constants.*;
 
 public class CategoriesTest extends BaseTest {
 
@@ -26,11 +25,25 @@ public class CategoriesTest extends BaseTest {
 
 
     @Test
-    public void categoriesMenuLayoutTest(){
+    public void categoriesMenuLayoutTest() {
         CategoriesMenu categories = new CategoriesMenu(driver);
         // not sure if good
         Assert.assertTrue(areElementsDisplayed(categories.getCategoriesMenu()));
     }
 
+    @Test
+    public void categoryNavigationTest() {
+
+        CategoriesMenu categories = new CategoriesMenu(driver);
+        ProductPage products = new ProductPage(driver);
+
+        clickElement(categories.books);
+        softAssert.assertTrue(compareWithExpected(categories.books.getText(), products.pageTitle.getText()));
+        driver.navigate().back();
+
+        clickElement(categories.computers);
+        softAssert.assertTrue(compareWithExpected(categories.computers.getText(), products.pageTitle.getText()));
+        driver.navigate().back();
+    }
 
 }
