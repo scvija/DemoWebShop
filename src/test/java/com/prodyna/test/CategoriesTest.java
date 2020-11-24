45aa9ceb928d679210775026fbc3aabe84448fe8
scvijanovic <stefan.cvijanovic@prodyna.com>
2020-11-24 22:45:36 +0100

    Adapted tests to new selenium service implementations

--- src/test/java/com/prodyna/test/CategoriesTest.java
+++ src/test/java/com/prodyna/test/CategoriesTest.java
@@ -3,7 +3,6 @@ package com.prodyna.test;
 import com.prodyna.configuration.BaseTest;
 import com.prodyna.pageObjects.CategoriesMenu;
 import com.prodyna.pageObjects.ProductPage;
-import org.junit.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,7 +16,7 @@ public class CategoriesTest extends BaseTest {
     @BeforeMethod
     public void initialize() throws IOException {
         driver = initializeDriver();
-        navigateToPage(homepageUrl());
+        navigateToPage(homepageUrl);
     }
 
     @AfterMethod
@@ -30,7 +29,7 @@ public class CategoriesTest extends BaseTest {
     public void categoriesMenuLayoutTest() {
         CategoriesMenu categories = new CategoriesMenu(driver);
         // not sure if good
-        Assert.assertTrue(areElementsDisplayed(categories.getCategoriesMenu()));
+        areElementsDisplayed(categories.getCategoriesElements());
     }
 
     @Test
@@ -40,11 +39,11 @@ public class CategoriesTest extends BaseTest {
         ProductPage products = new ProductPage(driver);
 
         clickElement(categories.books);
-        softAssert.assertTrue(compareWithExpected(categories.books.getText(), products.pageTitle.getText()));
+        softAssert.assertTrue(compareWithExpected(categories.books, products.pageTitle.getText()));
         driver.navigate().back();
 
         clickElement(categories.computers);
-        softAssert.assertTrue(compareWithExpected(categories.computers.getText(), products.pageTitle.getText()));
+        softAssert.assertTrue(compareWithExpected(categories.computers, products.pageTitle.getText()));
         driver.navigate().back();
 
         softAssert.assertAll();
@@ -56,7 +55,7 @@ public class CategoriesTest extends BaseTest {
         ProductPage products = new ProductPage(driver);
 
         clickElement(categories.apparelShoes);
-        softAssert.assertTrue(areElementsDisplayed(products.productSelectors));
+        areElementsDisplayed(products.getProductSelectors());
         softAssert.assertTrue(isElementDisplayed(products.nextPage));
 
         //could do it with a look, where to store it? point 12.
