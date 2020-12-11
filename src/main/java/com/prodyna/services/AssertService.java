427ecf8febd66da199f38d525a97dd3ded511e71
scvijanovic <stefan.cvijanovic@prodyna.com>
2020-12-11 21:44:44 +0100

    Added methods for asserting strings from a hashmap, asserting standard text and asserting containsIgnoreCase

--- src/main/java/com/prodyna/services/AssertService.java
+++ src/main/java/com/prodyna/services/AssertService.java
@@ -5,23 +5,25 @@ import org.openqa.selenium.WebElement;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
+import java.util.Map;
 
+import static com.prodyna.pageObjects.Search.SEARCH_PARTIAL_VALID;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class AssertService {
-    private WebDriver driver;
+public class AssertService extends  SeleniumService {
 
     public AssertService(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
+
 
     public void assertUrlContainsExpected(String expectedStringInUrl) {
         SeleniumService seleniumService = new SeleniumService(driver);
         String actualUrl = seleniumService.getCurrentUrl();
         assertTrue(actualUrl.contains(expectedStringInUrl), "Actual Url is: " + actualUrl);
     }
-    // TODO not soft assert
+    // TODO  soft assert only when there are more asserts in a test
     public void assertUrlEqualsToExpected(String expectedUrl) {
         SeleniumService seleniumService = new SeleniumService(driver);
         String actualUrl = seleniumService.getCurrentUrl();
@@ -80,6 +82,35 @@ public class AssertService {
         String elementText = element.getText();
         softAssert.assertTrue(elementText.equalsIgnoreCase(expectedText));
 
+        softAssert.assertAll();
+
+    }
+
+    public void assertFieldStandardTextEqualsText(WebElement element, String text) {
+        SoftAssert softAssert = new SoftAssert();
+        SeleniumService seleniumService = new SeleniumService(driver);
+
+        softAssert.assertEquals(seleniumService.getFieldText(element), text);
+
+        softAssert.assertAll();
+    }
+
+    public void assertElementTextContainsIgnoreCase(WebElement element, String text){
+        SoftAssert softAssert = new SoftAssert();
+        SeleniumService seleniumService = new SeleniumService(driver);
+
+        softAssert.assertTrue(seleniumService.textContainsIgnoreCase(element.getText(), text));
+
+        softAssert.assertAll();
+    }
+
+    public void assertTextFromMap(Map<String,String> map){
+        SoftAssert softAssert = new SoftAssert();
+
+        for(String actualText : map.keySet()){
+            String expectedText = map.get(actualText);
+            softAssert.assertEquals(actualText,expectedText, "These values are not equal: "+ actualText + " - " + expectedText);
+        }
         softAssert.assertAll();
     }
 
